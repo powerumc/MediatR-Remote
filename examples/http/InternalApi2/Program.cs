f7e160d6e1f52bd4430cea433efa96fd0c5ce999
Junil Um <umc@tripolygon.com>
2023-04-07 15:17:42 +0900

    feat: MediatR 12.0.1 support

--- examples/http/InternalApi2/Program.cs
+++ examples/http/InternalApi2/Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly(), typeof(HelloRemoteRequest).Assembly);
+var assemblies = new[] { Assembly.GetExecutingAssembly(), typeof(HelloRemoteRequest).Assembly };
+builder.Services.AddMediatR(serviceConfiguration => serviceConfiguration.RegisterServicesFromAssemblies(assemblies));
 builder.Services.AddRemoteMediatR("internal-api2");
 
 var app = builder.Build();
