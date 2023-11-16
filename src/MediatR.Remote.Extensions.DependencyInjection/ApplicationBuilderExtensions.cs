61213d230ee7fafbd28b868195f602f901ade37b
powerumc <powerumc@gmail.com>
2023-11-16 21:04:12 +0900

    chore: Rename UseMediatRemote to UseMediatRemote

--- src/MediatR.Remote.Extensions.DependencyInjection/ApplicationBuilderExtensions.cs
+++ src/MediatR.Remote.Extensions.DependencyInjection/ApplicationBuilderExtensions.cs
@@ -6,7 +6,7 @@ public static class ApplicationBuilderExtensions
     /// Use the mediator remote style of middleware.
     /// It needs <see cref="RoutingBuilderExtensions.UseRouter(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Routing.IRouter)"/>.
     /// </summary>
-    public static IApplicationBuilder UseMediatRemote(this IApplicationBuilder app,
+    public static IApplicationBuilder UseRemoteMediatR(this IApplicationBuilder app,
         Action<RemoteMediatorApplicationBuilder> builder)
     {
         builder?.Invoke(new RemoteMediatorApplicationBuilder(app));
@@ -17,7 +17,7 @@ public static class ApplicationBuilderExtensions
     /// <summary>
     /// Use the mediator remote style of .NET 6 or higher.
     /// </summary>
-    public static IEndpointRouteBuilder UseMediatRemote(this IEndpointRouteBuilder app, 
+    public static IEndpointRouteBuilder UseRemoteMediatR(this IEndpointRouteBuilder app, 
         Action<RemoteMediatorEndpointRouteBuilder> builder)
     {
         builder?.Invoke(new RemoteMediatorEndpointRouteBuilder(app));
